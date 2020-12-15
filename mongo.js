f7303d65a0ad8969c369ca0f1819b8169cf1c822
noorarytila <rytilanoora@gmail.com>
2020-12-15 10:13:23 +0200

    tehtävä 3.12 mongo db komentoriviltä tehty

---
+++ mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit()
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://puhelinluettelo_dev:${password}@cluster0.dados.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true })
+
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length === 3) {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name}  ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+
+if(process.argv.length === 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    const person = new Person({
+        name: name,
+        number: number
+    })
+
+    person.save().then(response => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+
+
+
